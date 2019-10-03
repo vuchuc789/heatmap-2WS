6ea1c5cb7c1472adb204b99e9486ab5a9ff3ee51
vuchuc781999 <37411808+vuchuc781999@users.noreply.github.com>
2019-10-04 01:59:01 +0700

    test push

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World !!!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port} !!!`);
 });
